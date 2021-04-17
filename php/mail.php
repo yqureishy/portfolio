c4083098135a6767aa89f14e2c7567a1ceda5ae3
yqureishy <yqureishy1993@gmail.com>
2021-04-17 11:37:24 -0500

    changed domain

--- php/mail.php
+++ php/mail.php
@@ -27,7 +27,7 @@ $toEmail = "yqureishy1993@gmail.com"; // Replace Your Email Address
   // Sender's Email
 --------------------------------------------- */
 
-$fromEmail = "no-reply@website.com";  // Replace Company's Email Address (preferably currently used Domain Name)
+$fromEmail = "no-reply@gmail.com";  // Replace Company's Email Address (preferably currently used Domain Name)
 $fromName = "Company Name"; // Replace Company Name
 
 
